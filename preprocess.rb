e8e3b752ae3e4c9728b53db5a0e4c7a680a6eecd
Anatoly Chernow <chertoly@gmail.com>
2017-09-11 23:17:01 +0300

    Dig deeper into the function body

--- preprocess.rb
+++ preprocess.rb
@@ -13,6 +13,12 @@ lf.def.class  #=> C::Block < C::Statement
 lf.def.smtms #=> C::NodeChain < C::NodeList
 lf.def.smtms.size #=> 2
 
+lf.def.stmts.first.class #=> C::Declaration < C::Node
+# VALUE Hello = rb_define_module("Hello");
+
+lf.def.stmts.last.class #=> C::ExpressionStatement < C::Statement
+# rb_define_singleton_method(Hello, "world", just_return_nil, 0);
+
 require 'pry'
 binding.pry
 puts ast
