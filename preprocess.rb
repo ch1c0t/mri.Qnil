86f62237657344fddbb63d3d1611a9001f6a2ef4
Anatoly Chernow <chertoly@gmail.com>
2017-09-11 23:10:57 +0300

    Document FunctionDef components

--- preprocess.rb
+++ preprocess.rb
@@ -5,6 +5,13 @@ code = IO.read path_to_file
 
 ast = C.parse code.lines[1..-1].join
 functions = ast.entities.grep C::FunctionDef
+lf = functions.last
+
+lf.type.class #=> C::Function < C::IndirectType
+lf.def.class  #=> C::Block < C::Statement
+
+lf.def.smtms #=> C::NodeChain < C::NodeList
+lf.def.smtms.size #=> 2
 
 require 'pry'
 binding.pry
